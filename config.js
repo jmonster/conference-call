767e12affd8387bf4a40ae524c3bf2ce5071e722
Johnny Domino <blah@hey.com>
2021-03-04 11:00:23 -0500

    use personal DO box for TURN server

--- config.js
+++ config.js
@@ -1,14 +1,23 @@
 export default {
   signalingServerUrl: 'https://satellite.jellystone.yoga',
   iceServers: [
-    // STUN
+    // TURN
     {
-      'urls': 'stun:stun.l.google.com:19302',
-      'url': 'stun:stun.l.google.com:19302'
-    },
-    {
-      'url': 'stun:global.stun.twilio.com:3478?transport=udp',
-      'urls': 'stun:global.stun.twilio.com:3478?transport=udp'
+      'urls': 'turn:turn.jellystone.yoga:3478?transport=udp',
+      'url': 'turn:turn.jellystone.yoga:3478?transport=udp',
+      username: 'webrtc',
+      credential: 'wubalubadubdub'
     }
+
+    // // STUN ONLY
+    // {
+    //   'urls': 'stun:stun.l.google.com:19302',
+    //   'url': 'stun:stun.l.google.com:19302'
+    // },
+
+    // {
+    //   'url': 'stun:global.stun.twilio.com:3478?transport=udp',
+    //   'urls': 'stun:global.stun.twilio.com:3478?transport=udp'
+    // }
   ]
 }
