8c64de0d50bdf2a4d5f53b959253af10a83f2451
Johnny Domino <blah@hey.com>
2021-03-04 11:24:20 -0500

    use twilio STUN instead

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ export default {
   signalingServerUrl: 'https://satellite.jellystone.yoga',
   iceServers: [
     // TURN
-    {
-      'urls': 'turn:turn.jellystone.yoga:3478?transport=udp',
-      'url': 'turn:turn.jellystone.yoga:3478?transport=udp',
-      username: 'webrtc',
-      credential: 'wubalubadubdub'
-    }
+    // {
+    //   'urls': 'turn:turn.jellystone.yoga:3478?transport=udp',
+    //   'url': 'turn:turn.jellystone.yoga:3478?transport=udp',
+    //   username: 'webrtc',
+    //   credential: 'wubalubadubdub'
+    // }
 
     // // STUN ONLY
     // {
@@ -15,9 +15,9 @@ export default {
     //   'url': 'stun:stun.l.google.com:19302'
     // },
 
-    // {
-    //   'url': 'stun:global.stun.twilio.com:3478?transport=udp',
-    //   'urls': 'stun:global.stun.twilio.com:3478?transport=udp'
-    // }
+    {
+      'url': 'stun:global.stun.twilio.com:3478?transport=udp',
+      'urls': 'stun:global.stun.twilio.com:3478?transport=udp'
+    }
   ]
 }
